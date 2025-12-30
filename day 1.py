f27ecb9061fc10cc803f03a304a283d82774112a
JDML-Dcoder107 <24-02573@g.batstate-u.edu.ph>
2025-12-30 20:43:22 +0800

    day 1 of Python Coding Challenge

---
+++ day 1.py
@@ -0,0 +1,6 @@
+name = "John Doe"
+age = 19
+CONSTANT_VALUE_OF_GWA = 3.14
+is_student = True
+weight = 49.89
+print(f"My name is {name}, I am {age} years old, my GWA is {CONSTANT_VALUE_OF_GWA}, it is {is_student} that I am a student, and my weight is {weight}.")
