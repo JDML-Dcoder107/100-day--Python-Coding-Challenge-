b35a7b30af063f1cd4c7bc77982c1443930e7e6f
JDML-Dcoder107 <24-02573@g.batstate-u.edu.ph>
2025-12-30 21:07:25 +0800

    day 2 of Python Coding Challenge

--- day 2.py
+++ day 2.py
@@ -2,24 +2,11 @@ print(" Calculator ".center(22, "="))
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 operation = input("Enter operation (+, -, *, /, //): ")
-match(operation):
-    case "+":
-        result = num1 + num2
-    case "-":
-        result = num1 - num2
-    case "*":
-        result = num1 * num2
-    case "/":
-        if num2 != 0:
-            result = num1 / num2
-        else:
-            result = "Error: Division by zero"
-    case "//":
-        if num2 != 0:
-            result = num1 // num2
-        else:
-            result = "Error: Division by zero"
-    case _:
-        result = "Error: Invalid operation"
+try:
+    result = eval(f"{num1} {operation} {num2}")
+except ZeroDivisionError as e:
+    result = "Error: Division by zero is not allowed."
+except ValueError as e:
+    result = "Error: Invalid input."
 
 print(f"The result is: {result}")
